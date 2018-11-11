8084d5e6f4ad107b5c1c43b5e8170d7bea827e0c
XeniaOlshanskaya <ks14012000@gmail.com>
2018-11-11 18:05:13 +0700

    process

--- project5.py
+++ project5.py
@@ -1,3 +1,20 @@
-with open('student.txt', 'r') as student_names:
-    #for student in student_names:
-        #marks = input("Введите через пробел текущие оценки ученика: {} ".format(student))
+student_names = open("student.txt", "r")
+result_txt = open("result.txt", "w")
+count = 0
+marks_sum = 0
+result_mark = 0
+result_rating = 0
+
+for student in student_names:
+    marks = input("Введите через пробел текущие оценки ученика: {} ".format(student))
+    marks = marks.split(" ")
+    for mark in marks:
+        count += 1
+        mark = int(mark)
+        marks_sum += mark
+    result_rating = round((marks_sum / count), 2)
+    result_mark = round(result_rating)
+    print("{} - rating: {}, mark: {} ".format(student, result_rating, result_mark), file=result_txt)
+    count = 0
+    marks_sum = 0
+
